21fe97dbeed0dac11058f315ce9da3518e9a0c17
Joakim12390 <joakim12390@hotmail.com>
2017-12-18 09:34:17 +0100

    Solution anagrams

--- del1/src/anagrams/Solution.cs
+++ del1/src/anagrams/Solution.cs
@@ -5,7 +5,20 @@ using System.Linq;
 class Solution {
 
     static int makingAnagrams(string s1, string s2){
-        // Complete this function
+        for(int i = 0; i < s1.Length; i++)
+			{
+				for (int j = 0; j < s2.Length; j++)
+				{
+					if(s1[i].Equals(s2[j]))
+					{
+						s1 = s1.Remove(i,1);
+						i -= 1;
+						s2 = s2.Remove(j,1);
+						break;
+					}
+				}
+			}
+		return s1.Length + s2.Length;
     }
 
     static void Main(String[] args) {
